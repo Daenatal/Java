c3467a1ceace426429d1c7f571489a7a5736835b
Daenatal <daenatal@yahoo.com>
2019-03-12 13:59:05 -0500

    Updating Basics files, almost complete.

--- JavaIntro/Basics/BasicJava.java
+++ JavaIntro/Basics/BasicJava.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class BasicJava {
     public void printNumbers() {
         for(int i = 1; i<256; i++) {
@@ -44,5 +45,66 @@ public class BasicJava {
         }
         int average = sum / targetArray.length;
         System.out.println(average);
+    }
+    public void generateOdd(ArrayList<Integer> y) {
+        for(int i = 1; i < 256; i++) {
+            if( (i % 2) != 0) {
+                y.add(i);
+            }
+        }
+        for(int i = 0; i < y.size(); i++) {
+            System.out.println(y.get(i));
+        }
+    }
+    public void filterArray(ArrayList<Integer> x, int y) {
+        for(int i = 0; i <x.size(); i++) {
+            if (x.get(i) < y) {
+                x.remove(i);
+            }
+        } //problem here is that for every iteration the size gets smaller after a removal
+        // a possible soluion would be to create a new array instead of just removing the old one.
+        for(int i = 0; i <x.size(); i++) {
+            if (x.get(i) < y) {
+                x.remove(i);
+            }
+        }
+    }
+    public void squareArray(ArrayList<Integer> x) {
+        for(int i = 0; i< x.size(); i++) {
+            int y = x.get(i) * x.get(i);
+            x.set(i , y);
+        }
+    }
+    public void onlyPositive(ArrayList<Integer> x) {
+        for(int i= 0; i< x.size(); i++) {
+            if (x.get(i) < 0) {
+                x.set(i , 0);
+            }
+        }
+    }
+    public int[] getValues(ArrayList<Integer> x) {
+        int[] minmaxavg = { 0, 0, 0};
+        int min = 0;
+        int max = 0;
+        int avg = 0;
+        for(int i = 0; i< x.size(); i++) {
+            if( i == 0) {
+                min = x.get(i);
+                max = x.get(i);
+                avg += x.get(i);
+            } else { 
+            if (x.get(i) < min) {
+                min = x.get(i);
+            } 
+            if (x.get(i) > max) {
+                max = x.get(i);
+            }
+                avg += x.get(i);
+            }
+        }
+        minmaxavg[0] = max;
+        minmaxavg[1] = min;
+        minmaxavg[2] = avg/x.size();
+        return minmaxavg;
     }
 }
--- JavaIntro/Basics/BasicJavaMain.java
+++ JavaIntro/Basics/BasicJavaMain.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 public class BasicJavaMain{
     public static void main(String[] args) {
         BasicJava run = new BasicJava();
@@ -12,5 +14,21 @@ public class BasicJavaMain{
         run.getAverage(myArray);
         int[] testArray= {2, 10, 3};
         run.getAverage(testArray);
+        ArrayList<Integer> myArray2 = new ArrayList<Integer>();
+        //run.generateOdd(myArray2);
+        ArrayList<Integer> myArray3 = new ArrayList<Integer>();
+        Collections.addAll(myArray3, -3, 50, -9 , 20, 11, -18, 200, 2, 5, -19);
+        System.out.println(myArray3);
+        //run.filterArray(myArray3, 10);
+        //run.squareArray(myArray3);
+        run.onlyPositive(myArray3);
+        System.out.println(myArray3);
+        ArrayList<Integer> minMax = new ArrayList<Integer>();
+        Collections.addAll(minMax, 1, 5, 10, -2);
+        System.out.println(minMax);
+        int[] b = run.getValues(minMax);
+        System.out.println(b[0] + " This is the Maximum value of your dataset");
+        System.out.println(b[1] + " This is the Minimum value of your dataset");
+        System.out.println(b[2] + " This is the Average value of your dataset");
     }
 }
